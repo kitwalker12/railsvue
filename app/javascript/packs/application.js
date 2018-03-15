299062b5cba22915f08c24bfc2792fd2a4386237
Aditya Raghuwanshi <adi.version1@gmail.com>
2018-03-15 16:02:24 -0700

    Remove default components

--- app/javascript/packs/application.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from '../components/app.vue'
-
-document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    el: 'App',
-    render: h => h(App)
-  })
-})
